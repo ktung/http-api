212a0333417c3277a9d1354a7c06f5073534e1e0
ptung <misuke01@gmail.com>
2021-04-14 13:41:04 -0400

    Add Json Controller

--- src/main/java/com/ktung/httpapi/controller/GreetingController.java
+++ src/main/java/com/ktung/httpapi/controller/GreetingController.java
@@ -3,7 +3,10 @@ package com.ktung.httpapi.controller;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.ktung.httpapi.model.Greeting;
+import com.ktung.httpapi.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +19,10 @@ public class GreetingController {
   @GetMapping("/greeting")
   public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
     return new Greeting(counter.incrementAndGet(), String.format(template, name));
+  }
+
+  @PostMapping("/greeting")
+  public Greeting greetingUser(@RequestBody User user) {
+    return new Greeting(counter.incrementAndGet(), String.format(template, user.getName()));
   }
 }
